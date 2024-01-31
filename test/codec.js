7999e3bf3ff7ddac7e282e9397f04d8824d31462
geoffbrucepayne <geoff.bruce-payne@inmarsat.com>
2024-01-30 23:22:29 -0500

    add codec

---
+++ test/codec.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+chai.config.includeStack = false;
+const expect = chai.expect;
+
+const { decodeMessage } = require('../lib/messageCodec');
+
+describe('#decodeMessage()', function () {
+  
+  const idpmsgTest = '/home/gbp/isatprojects/isatdatapro-api/test/fieldedge-iotdemo.idpmsg';
+
+  context('with idpmsg file', function() {
+    it('should decode the test message', function() {
+      const decoded = decodeMessage([255, 1], idpmsgTest, false);
+      expect(decoded).to.be.an('Object').with.keys('name', 'codecServiceId', 'codecMessageId', 'fields');
+      expect(decoded.name).to.equal('satelliteTelemetryGet');
+    })
+  });
+
+});
